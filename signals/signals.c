738c56673f95585c8445448d44015508f8c36aec
Loic Baecher <lbaecher@c1r3s3.42lausanne.ch>
2025-01-08 12:32:13 +0100

    add signals first try

---
+++ signals/signals.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbaecher <lbaecher@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 10:14:07 by lbaecher          #+#    #+#             */
+/*   Updated: 2025/01/08 10:29:01 by lbaecher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	sig_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		write(1, "HAHAHAHA", 1);
+	}
+}
+
+void	add_key_signals(void)
+{
+	struct	sigaction sig;
+
+	sig.sa_handler = &sig_handler;
+	sigaction(SIGINT, &sig, NULL);
+}
