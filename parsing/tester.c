27a76f1dbd66182b561f0e00f8ebd06e4920dad3
Amaury <loubry.amaury@gmail.com>
2025-01-10 16:04:50 +0100

    upgraded tester

--- parsing/tester.c
+++ parsing/tester.c
@@ -6,11 +6,43 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:04:03 by aloubry           #+#    #+#             */
-/*   Updated: 2025/01/10 15:13:26 by aloubry          ###   ########.fr       */
+/*   Updated: 2025/01/10 16:04:11 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void test_loop(char *command)
+{
+	printf("--------------------\ntesting on input : \x1B[35m %s\n\x1B[0m", command);
+    char *expanded_input = expand_variables_of_input(command);
+    printf("expanded input: %s\n", expanded_input);
+    t_list *tokenized_expanded_input = tokenize_input(expanded_input);
+    free(expanded_input);
+    int token_number = 1;
+    t_list *current = tokenized_expanded_input;
+    while (current)
+    {
+        printf("token %d: %s\n", token_number, (char *)current->content);
+        current = current->next;
+        token_number++;
+    }
+	if(!check_syntax_errors(tokenized_expanded_input))
+		printf("no syntax error\n");
+	else
+		return ;
+	remove_quotes(tokenized_expanded_input);
+	printf("removing quotes...\n");
+	token_number = 1;
+    current = tokenized_expanded_input;
+    while (current)
+    {
+        printf("token %d: %s\n", token_number, (char *)current->content);
+        current = current->next;
+        token_number++;
+    }
+    ft_lstclear(&tokenized_expanded_input, free);
+}
 
 void run_parsing_tests(void)
 {
@@ -40,38 +72,17 @@ void run_parsing_tests(void)
         "echo \"Special characters: !@#$%^&*()_+\"",
 		"echo 'Hello \"World\"'",
 		"echo \"Hello 'World'\"",
+		"\"\"",
+		"''",
+		"echo \"\"",
         ""
     };
 
-    for (int i = 0; test_commands[i][0] != '\0'; i++)
-    {
-		printf("--------------------\ntesting on input : \x1B[35m %s\n\x1B[0m", test_commands[i]);
-        char *expanded_input = expand_variables_of_input(test_commands[i]);
-        printf("expanded input: %s\n", expanded_input);
-        t_list *tokenized_expanded_input = tokenize_input(expanded_input);
-        free(expanded_input);
-        int token_number = 1;
-        t_list *current = tokenized_expanded_input;
-        while (current)
-        {
-            printf("token %d: %s\n", token_number, (char *)current->content);
-            current = current->next;
-            token_number++;
-        }
-		if(!check_syntax_errors(tokenized_expanded_input))
-			printf("no syntax error\n");
-		else
-			continue ;
-		remove_quotes(tokenized_expanded_input);
-		printf("removing quotes...\n");
-		token_number = 1;
-        current = tokenized_expanded_input;
-        while (current)
-        {
-            printf("token %d: %s\n", token_number, (char *)current->content);
-            current = current->next;
-            token_number++;
-        }
-        ft_lstclear(&tokenized_expanded_input, free);
-    }
+	int i = 0;
+	while(test_commands[i][0])
+	{
+		test_loop(test_commands[i]);
+		i++;
+	}
+	test_loop(test_commands[i]);
 }
