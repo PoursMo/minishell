b11cbb8623152af46754360d0767c63698df211b
Loic Baecher <>
2025-01-14 16:53:48 +0100

    echo works

--- headers/builtins.h
+++ headers/builtins.h
@@ -6,13 +6,13 @@
 /*   By: lbaecher <lbaecher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:22:22 by lbaecher          #+#    #+#             */
-/*   Updated: 2025/01/09 17:48:45 by lbaecher         ###   ########.fr       */
+/*   Updated: 2025/01/14 16:53:35 by lbaecher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-void	ft_echo(int fd_out, int new_line_flag, int append, char *str);
+void	ft_echo(int new_line_flag, char *str);
 
 #endif
--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lbaecher <lbaecher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 13:44:35 by lbaecher          #+#    #+#             */
-/*   Updated: 2025/01/03 19:53:33 by aloubry          ###   ########.fr       */
+/*   Updated: 2025/01/14 16:53:10 by lbaecher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 int	main()
 {
-	ft_putstr_fd("test", 1);
+	int	fd;
+
+	fd = open("builtins/output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644 );
+	dup2(fd, STDOUT_FILENO);
+
+	//ft_echo(1, "hello");
+	//Output.txt should contain "Hello" and another line
+
+	ft_echo(0, "hello");
+	//Output.txt should contain "Hello"
 	return (0);
 }
