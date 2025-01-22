9d0a6ef6fa4d46ebe65c34e4cac1f0ea7ac4234b
Amaury <loubry.amaury@gmail.com>
2025-01-22 12:50:33 +0100

    fixed empty input

--- minishell.c
+++ minishell.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbaecher <lbaecher@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 13:44:35 by lbaecher          #+#    #+#             */
-/*   Updated: 2025/01/22 11:51:58 by lbaecher         ###   ########.fr       */
+/*   Updated: 2025/01/22 12:48:25 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int is_empty_input(char *input)
+{
+	while(ft_isspace(*input))
+		input++;
+	if(!*input)
+		return (1);
+	return (0);
+}
+
 int main()
 {
 	char *input;
@@ -19,7 +29,7 @@ int main()
 	while(1)
 	{
 		input = readline(">");
-		if (parse_input(input, &tokens) == -1) // gerer readline vide
+		if (is_empty_input(input) || parse_input(input, &tokens) == -1)
 			continue ;
 		int tmp_stdin = dup(STDIN_FILENO);
 		int tmp_stdout = dup(STDOUT_FILENO);
