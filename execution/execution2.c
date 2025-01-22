bd22d3f43a3b1de9fa777e50ccb00b7b4083671c
Amaury <loubry.amaury@gmail.com>
2025-01-22 14:05:30 +0100

    updating execution

--- execution/execution2.c
+++ execution/execution2.c
@@ -6,7 +6,11 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 12:41:57 by aloubry           #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2025/01/21 16:12:28 by aloubry          ###   ########.fr       */
+=======
+/*   Updated: 2025/01/22 13:43:40 by aloubry          ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +61,14 @@ char	**get_cmd_args(t_list *cmd, t_list *end)
 
 void	handle_builtin(t_list *cmd_ptr, t_list *pipe_ptr)
 {
+<<<<<<< Updated upstream
 	fprintf(stderr, "Executing builtin command: %s\n", (char *)cmd_ptr->content); // debug
 	fprintf(stderr, "pipe_ptr: %s\n", (char *)pipe_ptr->content); // debug
 	// size_t cmd_len;
+=======
+	size_t cmd_len;
+	char	**cmd_args;
+>>>>>>> Stashed changes
 
 	// cmd_len = ft_strlen(cmd_ptr->content);
 	// if (ft_strlen("echo") == cmd_len && !strncmp(cmd_ptr->content, "echo", cmd_len))
@@ -86,12 +95,9 @@ void	handle_non_builtin(t_list *cmd_ptr, t_list *pipe_ptr)
 	cmd = find_cmd_path(cmd_ptr->content);
 	if (!cmd)
 		exit(EXIT_FAILURE);
-	// fprintf(stderr, "full cmd: %s\n", cmd); // debug
 	cmd_args = get_cmd_args(cmd_ptr, pipe_ptr);
 	if (!cmd_args)
 		exit(EXIT_FAILURE);
-	// for (int j = 0; cmd_args[j]; j++) // debug
-		// fprintf(stderr, "cmd_args[%d]: %s\n", j, cmd_args[j]); // debug
 	if (execve(cmd, cmd_args, __environ) == -1)
 	{
 		perror("execve");
--- minishell.c
+++ minishell.c
@@ -6,7 +6,11 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 13:44:35 by lbaecher          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2025/01/22 12:48:25 by aloubry          ###   ########.fr       */
+=======
+/*   Updated: 2025/01/22 14:00:11 by aloubry          ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +29,16 @@ int main()
 {
 	char *input;
 	t_list *tokens;
+<<<<<<< Updated upstream
 
+=======
+	char	**new_env;
+	t_list *pids;
+
+	new_env = create_new_env(envp);
+	set_minishell_env(envp);
+	pids = NULL;
+>>>>>>> Stashed changes
 	while(1)
 	{
 		input = readline(">");
@@ -33,7 +46,8 @@ int main()
 			continue ;
 		int tmp_stdin = dup(STDIN_FILENO);
 		int tmp_stdout = dup(STDOUT_FILENO);
-		execute_tokens(tokens);
+		execute_tokens(tokens, &pids);
+		wait_for_processes(pids);
 		dup2(tmp_stdin, STDIN_FILENO);
 		dup2(tmp_stdout, STDOUT_FILENO);
 		ft_lstclear(&tokens, free);
