d1754b0276f9bb1f591534cb5258af7fa6965488
Amaury Loubry <aloubry@c1r15s6.42lausanne.ch>
2025-01-23 17:27:44 +0100

    fixed here_doc

--- utils/std_streams.c
+++ utils/std_streams.c
@@ -6,13 +6,13 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 14:46:47 by aloubry           #+#    #+#             */
-/*   Updated: 2025/01/23 12:20:39 by aloubry          ###   ########.fr       */
+/*   Updated: 2025/01/23 17:24:59 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	update_std_streams(int mode)
+static int	*update_std_streams(int mode)
 {
 	static int	std_streams[2];
 
@@ -21,28 +21,37 @@ static int	update_std_streams(int mode)
 		std_streams[STDIN_FILENO] = dup(STDIN_FILENO);
 		std_streams[STDOUT_FILENO] = dup(STDOUT_FILENO);
 		if (std_streams[STDIN_FILENO] == -1 || std_streams[STDOUT_FILENO] == -1)
-			return (perror("dup"), -1);
+			return (perror("dup"), NULL);
 	}
 	else if (mode == 1)
 	{
 		if (dup2(std_streams[STDIN_FILENO], STDIN_FILENO) == -1)
-			return (perror("dup2"), -1);
+			return (perror("dup2"), NULL);
 		if (dup2(std_streams[STDOUT_FILENO], STDOUT_FILENO) == -1)
-			return (perror("dup2"), -1);
+			return (perror("dup2"), NULL);
 		if (close(std_streams[STDIN_FILENO]) == -1)
-			return (perror("close"), -1);
+			return (perror("close"), NULL);
 		if (close(std_streams[STDOUT_FILENO]) == -1)
-			return (perror("close"), -1);
+			return (perror("close"), NULL);
 	}
-	return (0);
+	return (std_streams);
+}
+
+int *get_std_streams(void)
+{
+	return (update_std_streams(-1));
 }
 
 int	reset_std_streams(void)
 {
-	return (update_std_streams(1));
+	if (!update_std_streams(1))
+		return (-1);
+	return (0);
 }
 
 int	save_std_streams(void)
 {
-	return (update_std_streams(0));
+	if (!update_std_streams(0))
+		return (-1);
+	return (0);
 }
