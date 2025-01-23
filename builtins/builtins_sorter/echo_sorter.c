d6eb0cdc74c9a29b8bf9bc9a38d40ffaffe36c75
Loïc Baecher <loicbaecher@users.noreply.github.com>
2025-01-23 13:45:18 +0100

    fix echo

--- builtins/builtins_sorter/echo_sorter.c
+++ builtins/builtins_sorter/echo_sorter.c
@@ -6,38 +6,47 @@
 /*   By: loicbaecher <loicbaecher@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:39:48 by lbaecher          #+#    #+#             */
-/*   Updated: 2025/01/23 13:18:21 by loicbaecher      ###   ########.fr       */
+/*   Updated: 2025/01/23 13:44:03 by loicbaecher      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	echo_counter(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
+
 int	echo_sorter(char **args)
 {
-	int	count;
+	int	i;
 
-	count = 1;
-	if (!args[1])
-		return(printf("\n"), 0);
+	if (echo_counter(args) == 1)
+		return (printf("\n"), 0);
 	if (ft_strlen(args[1]) == 2 && !(ft_strncmp(args[1], "-n", 2)))
 	{
-		count++;
-		while (args[count + 1] != NULL)
+		i = 2;
+		while (args[i])
 		{
-			ft_echo(0, args[count]);
-			printf(" ");
-			count++;
+			ft_echo(0, args[i++]);
+			if (args[i])
+				printf(" ");
 		}
-		ft_echo(0, args[count]);
 	}
 	else
 	{
-		while (args[count + 1])
+		i = 1;
+		while (args[i + 1])
 		{
-			ft_echo(0, args[count++]);
+			ft_echo(0, args[i++]);
 			printf(" ");
 		}
-		ft_echo(1, args[count]);
+		ft_echo(1, args[i]);
 	}
 	return (1);
 }
--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbaecher <lbaecher@student.42.fr>          +#+  +:+       +#+        */
+/*   By: loicbaecher <loicbaecher@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:22:17 by lbaecher          #+#    #+#             */
-/*   Updated: 2025/01/22 14:37:44 by lbaecher         ###   ########.fr       */
+/*   Updated: 2025/01/23 13:34:16 by loicbaecher      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	ft_echo(int new_line_flag, char *str)
 {
-	if (new_line_flag)
-		printf("%s\n", str);
+	if (new_line_flag && !str)
+		return (printf("\n"), (void ) NULL);
+	if (new_line_flag && str)
+		return (printf("%s\n", str), (void ) NULL);
+	if (!new_line_flag && !str)
+		return ;
 	else
-		printf("%s", str);
+		return (printf("%s", str), (void ) NULL);
 }
