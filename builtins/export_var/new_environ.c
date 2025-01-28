bf789cd23e62c8d5dc6cc83a908135fed1ad4b8b
Loic Baecher <lbaecher@c1r16s3.42lausanne.ch>
2025-01-28 13:13:17 +0100

    add control for empty env

--- builtins/export_var/new_environ.c
+++ builtins/export_var/new_environ.c
@@ -6,7 +6,7 @@
 /*   By: lbaecher <lbaecher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 10:17:53 by lbaecher          #+#    #+#             */
-/*   Updated: 2025/01/21 11:28:01 by lbaecher         ###   ########.fr       */
+/*   Updated: 2025/01/28 13:06:39 by lbaecher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ char	**create_new_env(char **old_env)
 {
 	char	**new_env;
 
+	if (!old_env)
+	{
+		new_env = NULL;
+		return (new_env);
+	}
 	new_env = malloc_copy_env(old_env);
 	return (new_env);
 }
